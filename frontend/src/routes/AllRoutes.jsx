f516ccc0928061452b346330975df9b0ef1e1e03
Suman Biswas <sumanmanner@gmail.com>
2023-03-28 16:21:43 +0530

    updated

---
+++ frontend/src/routes/AllRoutes.jsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const AllRoutes = () => {
+  return <Box>AllRoutes</Box>;
+};
+
+export default AllRoutes;
---
+++ frontend/src/utils/Theme.js
@@ -0,0 +1,6 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+export const Theme = () => {
+  return <Box>Theme</Box>;
+};
