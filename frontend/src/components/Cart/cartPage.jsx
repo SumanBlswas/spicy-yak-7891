3a0e0a63dd2fa7b6ff4c76dd1f149be9dd20cac1
Rishi <rishirajbhardwaj20@gmail.com>
2023-03-30 13:21:04 +0530

    All Data of cart folder

---
+++ frontend/src/components/Cart/cartPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Box, Heading} from "@chakra-ui/react";
+
+const cartPage = () => {
+  return (
+    <Box>
+        <Heading>Chakra UI</Heading>
+    </Box>
+  )
+}
+
+export default cartPage
