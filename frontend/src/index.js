28faf9757b76018d85c44b60981917f6167988f7
Rishi <rishirajbhardwaj20@gmail.com>
2023-03-30 14:01:47 +0530

    All commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
